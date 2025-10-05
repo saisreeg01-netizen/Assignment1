323725aa09372735c400866c00ab2e76354d1951
Sai <saisreeg01@gmail.com>
2025-10-05 12:33:24 +0530

    Intial commit

---
+++ starts with hello and ends with world.py
@@ -0,0 +1,7 @@
+string = "Hello World"
+
+starts_with_hello = string.startswith("Hello")
+print(f"Starts with 'Hello': {starts_with_hello}")
+
+ends_with_world = string.endswith("World")
+print(f"Ends with 'World': {ends_with_world}")
